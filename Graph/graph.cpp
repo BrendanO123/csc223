9084caaccfdc181ef0acf75adf019cd7a156d18e
Archonic <55026152+Archonic944@users.noreply.github.com>
2026-03-24 11:17:17 -0400

    Add Graph Creation Wizard™

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -75,10 +75,44 @@ void Graph<t> :: display(){
         t val = iterate->first;
         Node<t>* node = iterate->second;
         vector<Node<t>*> adj = node->adjacentNodes;
-        cout << val << " has " << adj.size() << " neighbors:" << endl;
+        cout << val << " has " << adj.size() << (adj.size() == 1 ? " neighbor" : " neighbors") << endl;
         for(int i = 0; i<adj.size(); i++){
             Node<t>* n = adj[i];
             cout << " " << n->data << endl;
+        }
+    }
+}
+
+int main(){
+    Graph<int> graph;
+    cout << "Welcome to the Graph Creation Wizard™." << endl;
+    char input;
+    int node1, node2;
+    while(true){
+        cout << "Either add a node [A] or connect a node [C]." << endl;
+        cout << "When done, type Q to display your graph." << endl;
+        cin >> input;
+        if(input == 'A'){
+            cout << "Enter the value of the node you want to add." << endl;
+            cin >> node1;
+            if(graph.insertNode(node1)){
+                cout << "Node added successfully." << endl;
+            } else {
+                cout << "Node already exists." << endl;
+            }
+        } else if(input == 'C'){
+            cout << "Enter the values of the nodes you want to connect." << endl;
+            cin >> node1 >> node2;
+            if(graph.connectNodes(node1, node2)){
+                cout << "Nodes connected successfully." << endl;
+            } else {
+                cout << "One or both nodes do not exist." << endl;
+            }
+        } else if(input == 'Q'){
+            graph.display();
+            break;
+        } else {
+            cout << "Invalid input. Please enter either A for add or C for connect." << endl; 
         }
     }
 }
